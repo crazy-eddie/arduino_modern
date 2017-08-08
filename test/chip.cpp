f074cbe0b2d61b3daae7774bd775473a02015211
Noah Roberts <noah.roberts@hushmail.com>
2017-08-07 19:00:18 -0700

    just a beginning.

---
+++ test/chip.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE chip
+#include <boost/test/unit_test.hpp>
+
+#include "../include/hardware/arch/generic/chip.hpp"
+#include "../include/hardware/chip/atmega328p.hpp"
+
+BOOST_AUTO_TEST_CASE(blah)
+{
+}
